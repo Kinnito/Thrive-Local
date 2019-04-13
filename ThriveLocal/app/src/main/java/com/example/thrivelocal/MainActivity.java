42591a0d3b84641a9b5a48c6820535adc8151ee6
Jessica Birchfield <birchj4@cs.washington.edu>
2019-04-13 16:58:18 -0700

    final

--- ThriveLocal/app/src/main/java/com/example/thrivelocal/MainActivity.java
+++ ThriveLocal/app/src/main/java/com/example/thrivelocal/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.searchresults:
                 startActivity(new Intent(this, SearchResults.class));
                 return true;
+            case R.id.map:
+                startActivity(new Intent(this, SearchResults.class));
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
