6ca716de824f0871a326d69e02c965806d598f6c
Jessica Birchfield <birchj4@cs.washington.edu>
2019-04-13 15:48:25 -0700

    added basic menu

--- ThriveLocal/app/src/main/java/com/example/thrivelocal/SingleFragmentActivity.java
+++ ThriveLocal/app/src/main/java/com/example/thrivelocal/SingleFragmentActivity.java
@@ -8,28 +8,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
-    protected abstract Fragment createFragment();
-
-    @LayoutRes
-    protected int getLayoutResId()
-    {
-        return(R.layout.activity_fragment);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(getLayoutResId());
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = createFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-    }
+//    protected abstract Fragment createFragment();
+//
+//    @LayoutRes
+//    protected int getLayoutResId()
+//    {
+//        return(R.layout.activity_fragment);
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(getLayoutResId());
+//
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//
+//        if (fragment == null) {
+//            fragment = createFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragment_container, fragment)
+//                    .commit();
+//        }
+//    }
 
 }
